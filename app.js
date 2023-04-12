84086c7fbb98a3e582d2777ffafd22bad96a34eb
Priyanshi230 <priyasha2323@gmail.com>
2023-04-12 11:08:21 +0530

    Js file commit

--- app.js
+++ app.js
@@ -1,65 +1,94 @@
 const express = require('express');
 const app = express();
-const port = 3000;
-const middleware = require('./middleware');  
+const port = 3003;
+const middleware = require('./middleware')
 const path = require('path')
 const bodyParser = require("body-parser")
-const mongoose = require('./database');
+const mongoose = require("./database");
 const session = require("express-session");
 
- 
-//const mongodb = require('mongodb');
- 
-// pug is a easy to use templete 
- app.set("view engine","pug");
- // the view name becomes the file path, minus the root directory and the file extension.
- app.set("views","views"); 
- app.use(bodyParser.urlencoded({extended: false})); 
- // join the path and directory name
- app.use(express.static(path.join(__dirname,"public")))
+const server = app.listen(port, () => console.log("Server listening on port " + port));
+const io = require("socket.io")(server, { pingTimeout: 60000 });
 
- app.use(session ({
-    // secrets are used to hash the session 
-    // a string is used to hash here
-    secret: "priyanshilovespasta",
-    // resave forces the session to be saved
-    // resave - takes a Boolean value. It enables the session to be stored back to the session store, even if the session was never modified during the request
+app.set("view engine", "pug");
+app.set("views", "views");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(session({
+    secret: "bbq chips",
     resave: true,
-    // saveUninitialized : prevents it from saving the session if uninitilized
-    saveUninitialized:false
- }))
-
- 
-const server = app.listen(port,()=>{
-    console.log("Server listening on port " + port)
-});
-
+    saveUninitialized: false
+}))
 
 // Routes
 const loginRoute = require('./routes/loginRoutes');
 const registerRoute = require('./routes/registerRoutes');
-const logoutRoute = require('./routes/logoutRoutes.js');
+const logoutRoute = require('./routes/logout');
+const postRoute = require('./routes/postRoutes');
+const profileRoute = require('./routes/profileRoutes');
+const uploadRoute = require('./routes/uploadRoutes');
+const searchRoute = require('./routes/searchRoutes');
+const messagesRoute = require('./routes/messagesRoutes');
+const notificationsRoute = require('./routes/notificationRoutes');
 
-// API ROUTES
+// Api routes
 const postsApiRoute = require('./routes/api/posts');
+const usersApiRoute = require('./routes/api/users');
+const chatsApiRoute = require('./routes/api/chats');
+const messagesApiRoute = require('./routes/api/messages');
+const notificationsApiRoute = require('./routes/api/notifications');
 
+app.use("/login", loginRoute);
+app.use("/register", registerRoute);
+app.use("/logout", logoutRoute);
+app.use("/posts", middleware.requireLogin, postRoute);
+app.use("/profile", middleware.requireLogin, profileRoute);
+app.use("/uploads", uploadRoute);
+app.use("/search", middleware.requireLogin, searchRoute);
+app.use("/messages", middleware.requireLogin, messagesRoute);
+app.use("/notifications", middleware.requireLogin, notificationsRoute);
 
+app.use("/api/posts", postsApiRoute);
+app.use("/api/users", usersApiRoute);
+app.use("/api/chats", chatsApiRoute);
+app.use("/api/messages", messagesApiRoute);
+app.use("/api/notifications", notificationsApiRoute);
 
-app.use("/login",loginRoute );
-app.use("/register",registerRoute);
-app.use("/logout",logoutRoute);
+app.get("/", middleware.requireLogin, (req, res, next) => {
 
-app.use("/api/posts",postsApiRoute);
- 
-app.get("/",middleware.requireLogin,(req,res,next) => {
-
-      
-     var payload = {
-
-        pageTitle: "HOME",
+    var payload = {
+        pageTitle: "Home",
         userLoggedIn: req.session.user,
         userLoggedInJs: JSON.stringify(req.session.user),
-        
-     }
-    res.status(200).render("home",payload); 
+    }
+
+    res.status(200).render("home", payload);
+})
+
+io.on("connection", socket => {
+
+    socket.on("setup", userData => {
+        socket.join(userData._id);
+        socket.emit("connected");
+    })
+
+    socket.on("join room", room => socket.join(room));
+    socket.on("typing", room => socket.in(room).emit("typing"));
+    socket.on("stop typing", room => socket.in(room).emit("stop typing"));
+    socket.on("notification received", room => socket.in(room).emit("notification received"));
+
+    socket.on("new message", newMessage => {
+        var chat = newMessage.chat;
+
+        if(!chat.users) return console.log("Chat.users not defined");
+
+        chat.users.forEach(user => {
+            
+            if(user._id == newMessage.sender._id) return;
+            socket.in(user._id).emit("message received", newMessage);
+        })
+    });
+
 })
