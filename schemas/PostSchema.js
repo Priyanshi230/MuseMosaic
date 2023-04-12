1aa8a6ec57cf0a7a48a4ba74a11d686b6346489f
Priyanshi230 <priyasha2323@gmail.com>
2023-04-12 11:02:30 +0530

    Schema commit

--- schemas/PostSchema.js
+++ schemas/PostSchema.js
@@ -1,24 +1,17 @@
-const mongoose= require('mongoose');
+const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
-const PostSchema = new Schema({
-   content:{
-    type : String,
-    trim: true
-   },
-   postedBy:{
-    type: Schema.Types.ObjectId,
-    ref: "User"
-   },
-   pinned:Boolean,
-   // this is an array of users.
-   likes: [{type: Schema.Types.ObjectId,ref:'User'}],
-   retweetUsers: [{type: Schema.Types.ObjectId,ref:'User'}],
-   retweetData: {type: Schema.Types.ObjectId,ref:'Post'}
-},{timestamps: true});
-// timestamp :true >> will return the time (in milliseconds) at which the event was created.It is a read-only property of Event Interface 
 
-// using model we have exported our schema
-var Post = mongoose.model('Post',PostSchema);
-//
+const PostSchema = new Schema({
+    content: { type: String, trim: true },
+    postedBy: { type: Schema.Types.ObjectId, ref: 'User' },
+    pinned: Boolean,
+    likes: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+    retweetUsers: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+    retweetData: { type: Schema.Types.ObjectId, ref: 'Post' },
+    replyTo: { type: Schema.Types.ObjectId, ref: 'Post' },
+    pinned: Boolean
+}, { timestamps: true });
+
+var Post = mongoose.model('Post', PostSchema);
 module.exports = Post;
--- schemas/UserSchema.js
+++ schemas/UserSchema.js
@@ -1,51 +1,20 @@
-const mongoose= require('mongoose');
+const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
+
 const UserSchema = new Schema({
-    firstName:{
-        type: String,
-        required: true,
-        // trim is used to handel spaces alought we are handelling them already using trim 
-        trim:true   
-    },
-    lastName:{
-        type: String,
-        required:true,
-        trim:true
-    },
-    username: {
-        type: String,
-        required:true,
-        trim:true,
-        unique:true
-    },
+    firstName: { type: String, required: true, trim: true },
+    lastName: { type: String, required: true, trim: true },
+    username: { type: String, required: true, trim: true, unique: true },
+    email: { type: String, required: true, trim: true, unique: true },
+    password: { type: String, required: true },
+    profilePic: { type: String, default: "/images/profilePic.jpeg" },
+    coverPhoto: { type: String },
+    likes: [{ type: Schema.Types.ObjectId, ref: 'Post' }],
+    retweets: [{ type: Schema.Types.ObjectId, ref: 'Post' }],
+    following: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+    followers: [{ type: Schema.Types.ObjectId, ref: 'User' }]
+}, { timestamps: true });
 
-    email:{
-       type: String,
-       required:true,
-       trim:true,
-       unique:true
-    },
-
-    password:{
-        type: String,
-        required:true
-    },
-
-    
-    profilePic:{
-        type:String,
-        default: "/images/profilePic.png"
-    },
-    // this is an array of all the post that a user has liked.
-    likes: [{type: Schema.Types.ObjectId,ref:'Post'}],
-    retweets: [{type: Schema.Types.ObjectId,ref:'Post'}]
-
-
-},{timestamps: true});
-// timestamp :true >> will return the time (in milliseconds) at which the event was created.It is a read-only property of Event Interface 
-
-// using model we have exported our schema
-var User = mongoose.model('User',UserSchema);
-//
+var User = mongoose.model('User', UserSchema);
 module.exports = User;
